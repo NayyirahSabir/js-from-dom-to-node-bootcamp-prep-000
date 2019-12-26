bb774ecfb4915827f34fe5e238982a4d5a54e124
nsabir <awesomeartist15@gmail.com>
2019-12-26 00:47:01 +0000

    Automatically backed up by Learn

--- more_js/example_one.js
+++ more_js/example_one.js
@@ -1,5 +1,7 @@
 let person1 = "Peter"
 let person2 = "Withnail"
+let person3 = "Yeet"
 
 console.log( 'Hi, how are you, ' + person2 + '?' )
 console.log( 'Oh, I am doing just fine, ' + person1 + ', thanks!' )
+console.log( 'Hey what about me, ' + person1)
